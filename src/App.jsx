ee3c72c5a9053f2816f3dc5892700d958b508754
Ishan Bhardwaj <ishanbhardwaj177@gmail.com>
2026-01-23 00:51:55 +0530

    added nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Dock from './components/Dock'
 import "./app.scss"
+import Nav from './components/Nav'
 
 const App = () => {
   return (
     <div>
       <main>
         <Dock/>
+        <Nav/>
       </main>
     </div>
   )
