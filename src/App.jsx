fcf8b88661100a20b68980594c9e1244f75171f5
Ishan Bhardwaj <ishanbhardwaj177@gmail.com>
2026-01-23 00:14:32 +0530

    made the dock part

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Dock from './components/Dock'
+import "./app.scss"
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <Dock/>
+      </main>
+    </div>
+  )
+}
+
+export default App
